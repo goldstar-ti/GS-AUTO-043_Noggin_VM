d5ca6c1e3e1c84cc64dd314682f63ac21d76e5ea
PJ Gunasekera <pj.gunasekera@goldstartransport.com.au>
2025-10-22 10:14:28 +0800

    updated common/__init__ with hash_manager

--- common/__init__.py
+++ common/__init__.py
@@ -1,11 +1,14 @@
 from .config import ConfigLoader, ConfigurationError
 from .logger import LoggerManager
 from .database import DatabaseConnectionManager, DatabaseConnectionError
+from .hash_manager import HashManager, HashLookupError
 
 __all__ = [
     'ConfigLoader',
     'ConfigurationError',
     'LoggerManager',
     'DatabaseConnectionManager',
-    'DatabaseConnectionError'
+    'DatabaseConnectionError',
+    'HashManager',
+    'HashLookupError'
 ]
